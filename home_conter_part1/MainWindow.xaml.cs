dca9185c567fcb344eac680a61d9081bbe16f7c5
Artem <artemzhdanov1299@gmail.com>
2023-10-10 16:36:57 +0300

    Add project files.

---
+++ home_conter_part1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace home_conter_part1
+{
+    interface IView
+    {
+        void UpdateTextBlock(string text);
+    }
+    public partial class MainWindow : Window, IView
+    {
+            private Presenter _presenter;
+
+            public MainWindow()
+            {
+                InitializeComponent();
+
+                _presenter = new Presenter(this, new Model());
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                var button = sender as Button;
+                if (button != null)
+                {
+                    _presenter.ButtonClicked(button.Content.ToString());
+                }
+            }
+
+            private void Button_Clear(object sender, RoutedEventArgs e)
+            {
+                _presenter.ButtonClear();
+            }
+
+            private void Button_Ok(object sender, RoutedEventArgs e)
+            {
+                _presenter.OKButtonClicked();
+            }
+
+            public void UpdateTextBlock(string text)
+            {
+                textBlock.Text = text;
+            }
+
+
+    }
+}
---
+++ home_conter_part1/Presenter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace home_conter_part1
+{
+    class Presenter
+    {
+        private MainWindow _view;
+        private Model _model;
+
+        public Presenter(MainWindow view, Model model)
+        {
+            _view = view;
+            _model = model;
+        }
+
+        public void ButtonClicked(string buttonText)
+        {
+            string currentText = _view.textBlock.Text;
+            string updatedText = currentText + buttonText;
+            _view.UpdateTextBlock(updatedText);
+        }
+
+        public void ButtonClear()
+        {
+            _view.UpdateTextBlock(string.Empty);
+        }
+
+        public void OKButtonClicked()
+        {
+            MessageBox.Show("Good");
+        }
+    }
+}
